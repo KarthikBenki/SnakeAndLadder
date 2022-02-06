13f69de628252a1e3a1da596314a0de986d6b371
karthikbenki <karthikmc007@gmail.com>
2022-02-06 20:00:43 +0530

    Added: Dice roll to get 1 to 6 logic

--- src/com/snakeladdercomputation/SnakeAndLadderComputation.java
+++ src/com/snakeladdercomputation/SnakeAndLadderComputation.java
@@ -1,11 +1,17 @@
 package com.snakeladdercomputation;
+import java.util.Random;
 
 public class SnakeAndLadderComputation {
 	static int position = 0;
+	public static int diceRoll() {
+		Random random = new Random();
+		int dice = random.nextInt(6)+1;
+		return dice;
+	}
 
 	public static void main(String[] args) {
+		System.out.println("dice= "+diceRoll());
 		
-
 	}
 
 }
