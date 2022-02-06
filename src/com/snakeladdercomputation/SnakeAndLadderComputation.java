f39c5178135c61cfd6d5e10cf383ac564f8f187b
karthikbenki <karthikmc007@gmail.com>
2022-02-06 22:03:55 +0530

    Added: Play with Two players and display Who wins

--- src/com/snakeladdercomputation/SnakeAndLadderComputation.java
+++ src/com/snakeladdercomputation/SnakeAndLadderComputation.java
@@ -2,16 +2,20 @@ package com.snakeladdercomputation;
 import java.util.Random;
 
 public class SnakeAndLadderComputation {
-	static int position = 0;
+	static int playerOnePosition = 0;
+	static int playerTwoPosition = 0;
 	static final int NO_PLAY = 0;
 	static final int LADDER = 1;
 	static final int SNAKE = 2;
+	static final int PLAYER1 = 1;
+	static final int PLAYER2 = 2;
+	static int player = PLAYER1;
 	public static int diceRoll() {
 		Random random = new Random();
 		int dice = random.nextInt(6)+1;
 		return dice;
 	}
-	public static void optionCheck(int dice) {
+	public static int optionCheck(int dice,int position) {
 		Random random = new Random();
 		int option = random.nextInt(3);
 		if((option == LADDER) && (position+dice <= 100)) {
@@ -26,18 +30,58 @@ public class SnakeAndLadderComputation {
 		if(position < 0) {
 			position =0;
 		}
+		if(player == PLAYER1) {
+			playerOnePosition = position;
+		}
+		else if(player == PLAYER2) {
+			playerTwoPosition = position;
+		}
+		return option;
+	}
+	public static void playerPlayCheck( int option ) {
+		if(player == PLAYER1) {
+			if((option == SNAKE) || (option == NO_PLAY)) {
+				player = PLAYER2;
+			}
+		}
+		else if(player == PLAYER2) {
+			if((option == SNAKE) || (option == NO_PLAY)) {
+				player = PLAYER1;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		int diceCount = 0;
-		while(position<100) {
+		int dicePlayerOneCount = 0;
+		int dicePlayerTwoCount = 0;
+		int optionPlayerOne;
+		int optionPlayerTwo;
+		int option=0;
+		while(playerOnePosition < 100 && playerTwoPosition < 100) {
 		int dice = diceRoll();
-		System.out.println("dice= "+dice);
-		diceCount++;
-		optionCheck(dice);
-		System.out.println("position = "+position);
+//		System.out.println("dice= "+dice);
+		if(player == PLAYER1) {
+			optionPlayerOne = optionCheck(dice,playerOnePosition);
+			option = optionPlayerOne;
+			dicePlayerOneCount++;
 		}
-		System.out.println("diceCount = "+diceCount);
+		else if(player == PLAYER2) {
+			optionPlayerTwo = optionCheck(dice,playerTwoPosition);
+			option = optionPlayerTwo;
+			dicePlayerTwoCount++;
+		}
+		playerPlayCheck(option);
+		
+
+		}
+		System.out.println("playerOnePosition = "+playerOnePosition+"\nplayerTwoPosition = "+playerTwoPosition);
+		System.out.println("dicePlayerOneCount = "+dicePlayerOneCount+"\ndicePlayerTwoCount = "+dicePlayerTwoCount);
+		if (playerOnePosition == 100) {
+				System.out.println("Player 1 Wins");
+		}
+		else {
+				System.out.println("Player 2 Wins");
+		}
 		
 		
 	}
