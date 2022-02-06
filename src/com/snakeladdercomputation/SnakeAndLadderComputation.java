e656b88786ec2e5d90c072aa832b69b14dffc4c1
karthikbenki <karthikmc007@gmail.com>
2022-02-06 20:49:22 +0530

    Added: No_play_ladder_Snake logic

--- src/com/snakeladdercomputation/SnakeAndLadderComputation.java
+++ src/com/snakeladdercomputation/SnakeAndLadderComputation.java
@@ -3,14 +3,35 @@ import java.util.Random;
 
 public class SnakeAndLadderComputation {
 	static int position = 0;
+	static final int NO_PLAY = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
 	public static int diceRoll() {
 		Random random = new Random();
 		int dice = random.nextInt(6)+1;
 		return dice;
 	}
+	public static void optionCheck(int dice) {
+		Random random = new Random();
+		int option = random.nextInt(3);
+		if(option == LADDER) {
+			position += dice;
+		}
+		else if(option == SNAKE) {
+			position -= dice;
+		}
+		else {
+			position = position;
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("dice= "+diceRoll());
+		int dice = diceRoll();
+		System.out.println("dice= "+dice);
+		optionCheck(dice);
+		System.out.println("position = "+position);
+		
+		
 		
 	}
 
